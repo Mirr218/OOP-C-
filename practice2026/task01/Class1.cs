887b64fc2043a09edb6e846cad5f01d0cbb55f5c
Mirr218 <st.pt01@mail.ru>
2026-06-23 01:32:29 +0600

    feat: add string exstension for palindromes

--- practice2026/task01/Class1.cs
+++ practice2026/task01/Class1.cs
@@ -1,7 +1,19 @@
-﻿namespace task01
-{
-    public class Class1
-    {
+﻿using System.Text;
 
+namespace task01
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string str)
+        {
+            string input = str.ToLower();
+            StringBuilder sb = new StringBuilder();
+            foreach(char c in input)
+            {
+                if (char.IsWhiteSpace(c) || char.IsPunctuation(c)) { continue; }
+                else { sb.Append(c); }
+            }
+            return sb.ToString() == input;
+        }
     }
 }
