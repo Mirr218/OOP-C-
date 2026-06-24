99ade7fd1f6cdbbb03065e93f5d196faf555ce6d
wharrypa <st.pt01@mail.ru>
2026-06-24 18:04:40 +0600

    tests: add stub-funcs in Custom collection class for TDD

--- practice2026/task03/CustomCollection.cs
+++ practice2026/task03/CustomCollection.cs
@@ -9,4 +9,19 @@ public class CustomCollection<T> : IEnumerable<T>
     public void Add(T item) => _items.Add(item);
     public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public IEnumerable<T> GetReverseEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static IEnumerable<int> GenerateSequence(int start, int count)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> FilterAndSort(Func<T, bool> predicate, Func<T, IComparable> keySelector)
+    {
+        throw new NotImplementedException();
+    }
 }
