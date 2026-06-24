123fe303f51d92436e56d7937a7caa3e825efb7e
wharrypa <st.pt01@mail.ru>
2026-06-24 22:06:31 +0600

    feat: add ClassAnalyzer from task

---
+++ practice2026/task05/ClassAnalyzer.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+public class ClassAnalyzer
+{
+    private Type _type;
+
+    public ClassAnalyzer(Type type)
+    {
+        _type = type;
+    }
+}
