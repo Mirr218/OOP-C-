d467780ea509c3533e6ae4147edefb5e3fe591cc
wharrypa <st.pt01@mail.ru>
2026-06-24 19:42:18 +0600

    feat: stub-funcs in Cruiser and Fighter classes

---
+++ practice2026/task04/Cruiser.cs
@@ -0,0 +1,10 @@
+namespace task04;
+
+public class Cruiser : ISpaceship
+{
+    public int Speed => throw new NotImplementedException();
+    public int FirePower => throw new NotImplementedException();
+    public void MoveForward() => throw new NotImplementedException();
+    public void Rotate(int angle) => throw new NotImplementedException();
+    public void Fire() => throw new NotImplementedException();
+}
---
+++ practice2026/task04/Fighter.cs
@@ -0,0 +1,10 @@
+namespace task04;
+
+public class Fighter : ISpaceship
+{
+    public int Speed => throw new System.NotImplementedException();
+    public int FirePower => throw new System.NotImplementedException();
+    public void MoveForward() => throw new System.NotImplementedException();
+    public void Rotate(int angle) => throw new System.NotImplementedException();
+    public void Fire() => throw new System.NotImplementedException();
+}
