7509f1e423e2ae4d00d1d28fdcebdcb7a0da6bf2
wharrypa <st.pt01@mail.ru>
2026-06-29 14:18:22 +0600

    feat: add fields in classes

--- practice2026/task07/DisplayNameAttribute.cs
+++ practice2026/task07/DisplayNameAttribute.cs
@@ -2,5 +2,10 @@ namespace task07;
 
 public class DisplayNameAttribute : Attribute
 {
-
+    public string DisplayName { get; }
+    
+    public DisplayNameAttribute(string displayName)
+    {
+        DisplayName = displayName;
+    }
 }
--- practice2026/task07/VersionAttribute.cs
+++ practice2026/task07/VersionAttribute.cs
@@ -2,5 +2,12 @@ namespace task07;
 
 public class VersionAttribute : Attribute
 {
-
+    public int Major { get; }
+    public int Minor { get; }
+    
+    public VersionAttribute(int major, int minor)
+    {
+        Major = major;
+        Minor = minor;
+    }
 }
