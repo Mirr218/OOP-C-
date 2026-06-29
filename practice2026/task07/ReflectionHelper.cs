6cd53c9717589cec8f4702a6ce6875d41d184aff
wharrypa <st.pt01@mail.ru>
2026-06-29 15:15:38 +0600

    feat: realization of ReflectionHelper

--- practice2026/task07/ReflectionHelper.cs
+++ practice2026/task07/ReflectionHelper.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 
 namespace task07;
 
@@ -6,6 +7,36 @@ public static class ReflectionHelper
 {
     public static void PrintTypeInfo(Type type)
     {
+        var displayNameAttr = type.GetCustomAttribute<DisplayNameAttribute>();
+        if (displayNameAttr != null)
+        {
+            Console.WriteLine($"Класс: {displayNameAttr.DisplayName}");
+        }
 
+        var versionAttr = type.GetCustomAttribute<VersionAttribute>();
+        if (versionAttr != null)
+        {
+            Console.WriteLine($"Версия: {versionAttr.Major}.{versionAttr.Minor}");
+        }
+
+        Console.WriteLine("Методы:");
+        foreach (var method in type.GetMethods())
+        {
+            var methodDisplayName = method.GetCustomAttribute<DisplayNameAttribute>();
+            if (methodDisplayName != null)
+            {
+                Console.WriteLine($"  - {methodDisplayName.DisplayName}");
+            }
+        }
+
+        Console.WriteLine("Свойства:");
+        foreach (var property in type.GetProperties())
+        {
+            var propertyDisplayName = property.GetCustomAttribute<DisplayNameAttribute>();
+            if (propertyDisplayName != null)
+            {
+                Console.WriteLine($"  - {propertyDisplayName.DisplayName}");
+            }
+        }
     }
 }
