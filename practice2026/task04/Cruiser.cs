9f4dd976fdc3217b51a8894a989a3ce6d55833d5
Mirr218 <st.pt01@mail.ru>
2026-07-07 16:12:58 +0600

    feat: change implementation of ships movement

--- practice2026/task04/Cruiser.cs
+++ practice2026/task04/Cruiser.cs
@@ -4,7 +4,25 @@ public class Cruiser : ISpaceship
 {
     public int Speed => 50;
     public int FirePower => 100;
-    public void MoveForward() => Console.WriteLine("Move Move Move!!!");
-    public void Rotate(int angle) => Console.WriteLine($"Rotation on {angle} degrees");
-    public void Fire() => Console.WriteLine("KABOOOOM");
+    public double Position { get; private set; } = 0;
+    
+    public int CurrAngle { get; private set; } = 0;
+    public int Shots { get; private set; } = 0;
+
+    public void MoveForward()
+    {
+        Position+= Speed;
+    }
+    public void Rotate(int angle)
+    {
+        CurrAngle = (CurrAngle + angle) % 360;
+        if(CurrAngle < 0)
+        {
+            CurrAngle+=360;
+        }
+    }
+    public void Fire()
+    {
+        Shots++;
+    }
 }
--- practice2026/task04/Fighter.cs
+++ practice2026/task04/Fighter.cs
@@ -4,7 +4,25 @@ public class Fighter : ISpaceship
 {
     public int Speed => 100;
     public int FirePower => 17;
-    public void MoveForward() => Console.WriteLine("Move Move Move!!! FASTER");
-    public void Rotate(int angle) => Console.WriteLine($"Rotation on {angle} degrees");
-    public void Fire() => Console.WriteLine("Pew, Pew, Pew");
+    
+    public double Position { get; private set; } = 0;
+    public int CurrAngle { get; private set; } = 0;
+    public int Shots { get; private set; } = 0;
+
+    public void MoveForward()
+    {
+        Position+= Speed;
+    }
+    public void Rotate(int angle)
+    {
+        CurrAngle = (CurrAngle + angle) % 360;
+        if(CurrAngle < 0)
+        {
+            CurrAngle+=360;
+        }
+    }
+    public void Fire()
+    {
+        Shots++;
+    }
 }
