a280cfe0374d72a04a54a99ffbdd2663dc068c83
wharrypa <st.pt01@mail.ru>
2026-07-11 18:49:35 +0600

    test: first test and stub-func

---
+++ practice2026/task14/DefiniteIntegral.cs
@@ -0,0 +1,9 @@
+﻿namespace task14;
+
+public class DefiniteIntegral
+{
+    public static double Solve(double a, double b, Func<double, double> function, double step, int threadsNumber)
+    {
+        return null!;
+    }
+}
---
+++ practice2026/task14tests/DefiniteIntegralTests.cs
@@ -0,0 +1,17 @@
+﻿using task14;
+using Xunit;
+
+namespace task14tests;
+
+public class DefiniteIntegralTests
+{
+    [Fact]
+    public void Solve_ForLinearFunctionOnSymmetricSegment_ReturnsZero()
+    {
+        Func<double, double> function = x => x;
+
+        var result = DefiniteIntegral.Solve(-1, 1, function, 1e-4, 2);
+
+        Assert.Equal(0, result, 1e-4);
+    }
+}
