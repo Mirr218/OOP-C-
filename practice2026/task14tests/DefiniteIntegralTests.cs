3155103c26cb0aab44287a1f6903b0c5f8f07805
wharrypa <st.pt01@mail.ru>
2026-07-11 18:53:00 +0600

    test: tests from task

--- practice2026/task14tests/DefiniteIntegralTests.cs
+++ practice2026/task14tests/DefiniteIntegralTests.cs
@@ -14,4 +14,24 @@ public class DefiniteIntegralTests
 
         Assert.Equal(0, result, 1e-4);
     }
+
+    [Fact]
+    public void Solve_ForLinearFunctionFromZeroToFive_ReturnsTwelvePointFive()
+    {
+        Func<double, double> function = x => x;
+
+        var result = DefiniteIntegral.Solve(0, 5, function, 1e-6, 8);
+
+        Assert.Equal(12.5, result, 1e-5);
+    }
+
+    [Fact]
+    public void Solve_ForSinFunctionOnSymmetricSegment_ReturnsZero()
+    {
+        Func<double, double> function = x => Math.Sin(x);
+
+        var result = DefiniteIntegral.Solve(-1, 1, function, 1e-5, 8);
+
+        Assert.Equal(0, result, 1e-4);
+    }
 }
