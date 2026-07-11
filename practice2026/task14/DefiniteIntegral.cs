07c7cf40143abf9f5ff9ed89d76a984608bf2211
wharrypa <st.pt01@mail.ru>
2026-07-11 19:09:46 +0600

    refactor: split all section on threads

--- practice2026/task14/DefiniteIntegral.cs
+++ practice2026/task14/DefiniteIntegral.cs
@@ -4,7 +4,19 @@ public class DefiniteIntegral
 {
     public static double Solve(double a, double b, Func<double, double> function, double step, int threadsNumber)
     {
-        return CalculatePart(a, b, function, step);
+        double result = 0.0;
+        
+        double partLength = (b - a) / threadsNumber;
+
+        for (int i = 0; i < threadsNumber; i++)
+        {
+            double localA = a + i * partLength;
+            double localB = i == threadsNumber - 1 ? b : localA + partLength;
+
+            result += CalculatePart(localA, localB, function, step);
+        }
+
+        return result;
     }
 
     private static double CalculatePart(double a, double b, Func<double, double> function, double step)
