7b960fb9ff5b004a90e7d23d13032bc1aab50e9e
wharrypa <st.pt01@mail.ru>
2026-07-11 18:57:22 +0600

    feat: math implementation of integral solve

--- practice2026/task14/DefiniteIntegral.cs
+++ practice2026/task14/DefiniteIntegral.cs
@@ -4,6 +4,17 @@ public class DefiniteIntegral
 {
     public static double Solve(double a, double b, Func<double, double> function, double step, int threadsNumber)
     {
-        return null!;
+        double result = 0.0;
+
+        for (double x = a; x < b; x += step)
+        {
+            double nextX = Math.Min(x + step, b);
+            double heightLeft = function(x);
+            double heightRight = function(nextX);
+
+            result += (heightLeft + heightRight) / 2.0 * (nextX - x);
+        }
+
+        return result;
     }
 }
