0798b7a742275dcf34b58f3c3eda28586e6fbc78
wharrypa <st.pt01@mail.ru>
2026-07-11 19:28:24 +0600

    test: additional tests for task

--- practice2026/task14tests/DefiniteIntegralTests.cs
+++ practice2026/task14tests/DefiniteIntegralTests.cs
@@ -34,4 +34,46 @@ public class DefiniteIntegralTests
 
         Assert.Equal(0, result, 1e-4);
     }
+
+    [Fact]
+    public void Solve_ForConstantFunction_ReturnsRectangleArea()
+    {
+        Func<double, double> function = _ => 2;
+
+        var result = DefiniteIntegral.Solve(0, 3, function, 1e-4, 4);
+
+        Assert.Equal(6, result, 1e-4);
+    }
+
+    [Fact]
+    public void Solve_WithSingleThread_ReturnsCorrectResult()
+    {
+        Func<double, double> function = x => x;
+
+        var result = DefiniteIntegral.Solve(0, 2, function, 1e-5, 1);
+
+        Assert.Equal(2, result, 1e-4);
+    }
+
+    [Fact]
+    public void Solve_WithNullFunction_ThrowsException()
+    {
+        Assert.Throws<ArgumentNullException>(() => DefiniteIntegral.Solve(0, 1, null!, 1e-4, 2));
+    }
+
+    [Fact]
+    public void Solve_WithZeroStep_ThrowsException()
+    {
+        Func<double, double> function = x => x;
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => DefiniteIntegral.Solve(0, 1, function, 0, 2));
+    }
+
+    [Fact]
+    public void Solve_WithNegativeThreadsNumber_ThrowsException()
+    {
+        Func<double, double> function = x => x;
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => DefiniteIntegral.Solve(0, 1, function, 1e-4, -1));
+    }
 }
