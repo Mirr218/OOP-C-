0a2279d5ed35bf66e2a0ade7901e538d964be156
wharrypa <st.pt01@mail.ru>
2026-07-16 15:02:18 +0600

    feat: port server thread and scheduler to task19

---
+++ practice2026/task19/RoundRobinScheduler.cs
@@ -0,0 +1,25 @@
+namespace task19;
+
+public class RoundRobinScheduler : IScheduler
+{
+    private readonly Queue<ICommand> _commands = new();
+    private readonly object _lock = new();
+
+    public bool HasCommand()
+    {
+        lock (_lock) return _commands.Count > 0;
+    }
+
+    public ICommand Select()
+    {
+        lock (_lock)
+        {
+            return _commands.Dequeue();
+        }
+    }
+
+    public void Add(ICommand cmd)
+    {
+        lock (_lock) _commands.Enqueue(cmd);
+    }
+}
